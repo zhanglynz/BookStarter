93eb38b4cba879fcb30a36fb2ef90130871ecf92
L Zhang <lyzhang10@gmail.com>
2022-12-20 12:36:38 +1300

    initial commit

---
+++ R/start_book.R
@@ -0,0 +1,32 @@
+#' Start Book
+#'
+#' @return None
+#' @export
+#'
+start_book <- function()
+{# create docs folder
+ if(!dir.exists("./docs")) dir.create("./docs")
+
+ # copy .nojekyll into ./docs
+ file.copy(
+    from = system.file("extdata/.nojekyll", package = "BookStarter"),
+    to = "./docs",
+    overwrite = TRUE,
+    recursive = TRUE,
+    copy.mode = TRUE
+  )
+ # copy _bookdown.yml
+ file.copy(
+   from = system.file("extdata/_bookdown.yml", package = "BookStarter"),
+   to = ".",
+   recursive = TRUE,
+   copy.mode = TRUE
+  )
+ # copy index.Rmd
+ file.copy(
+   from = system.file("extdata/index.Rmd", package = "BookStarter"),
+   to = ".",
+   recursive = TRUE,
+   copy.mode = TRUE
+ )
+}
