851d60c932502d60bec71aa30026675fb3239dab
L Zhang <lyzhang10@gmail.com>
2024-01-03 17:47:32 +1300

    give q_start_book parameters

--- R/Q_start_book.R
+++ R/Q_start_book.R
@@ -1,9 +1,12 @@
 #' Start Quarto Book
 #'
+#' @param author author of the book, a character
+#' @param title title of the book, a character
 #' @return TRUE, if successfully start a book
 #' @export
 #'
-q_start_book <- function()
+q_start_book <- function(author = "Lingyun Zhang",
+                         title = "My New Book")
 {# create docs folder
  if(!dir.exists("./docs")) dir.create("./docs")
 
@@ -16,12 +19,16 @@ q_start_book <- function()
     copy.mode = TRUE
   )
  # copy _quarto.yml
- file.copy(
-   from = system.file("extdata/_quarto.yml", package = "BookStarter"),
-   to = ".",
-   recursive = TRUE,
-   copy.mode = TRUE
-  )
+ fn <- system.file("extdata/_quarto.yml", package = "BookStarter")
+ fr <- file(fn, open = "rt")
+
+ the_lines <- readLines(fr, n = -1)
+
+ the_lines[6] <- gsub('My book', title, the_lines[6])
+ the_lines[7] <- gsub('Lingyun Zhang', author, the_lines[7])
+
+ write(the_lines, file = "./_quarto.yml")
+ close(fr)
  # copy index.qmd
  file.copy(
    from = system.file("extdata/index.qmd", package = "BookStarter"),
